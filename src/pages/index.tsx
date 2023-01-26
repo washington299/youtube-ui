1635d4a440efe17302aa48432299975a3938a09a
Washington Campos <washingtoncampos9@gmail.com>
2023-01-25 22:14:26 -0300

    chore: set up prettier-plugin-tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-blue-400">Youtube clone</div>;
+  return (
+    <div className="bg-slate-200 text-center text-3xl text-blue-400 hover:text-sm sm:bg-black sm:text-yellow-300 md:text-blue-600">
+      Youtube clone
+    </div>
+  );
 }
