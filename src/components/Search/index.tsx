8033a306f414e4602ae95bd746373aeaa05789dd
Washington Campos <washingtoncampos9@gmail.com>
2023-01-27 19:51:53 -0300

    feat: turn Search input into a controlled component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { MicrophoneIcon } from '@heroicons/react/24/solid';
 
 export const Search = () => {
+	const [inputValue, setInputValue] = useState('');
+
 	return (
 		<>
-			<input type="text" placeholder="Search" />
+			<input
+				type="text"
+				placeholder="Search"
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+			/>
 			<button>
 				<MagnifyingGlassIcon className="h-6 w-6 text-gray-500 dark:text-white" />
 			</button>
