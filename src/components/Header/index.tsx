f197815b3a958956aab1c31a6ddfeb5a4e55e5c3
Washington Campos <washingtoncampos9@gmail.com>
2023-01-27 16:25:28 -0300

    feat: create header right part content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,37 @@
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import {
+	Bars3Icon,
+	MagnifyingGlassIcon,
+	EllipsisVerticalIcon,
+	UserCircleIcon,
+} from '@heroicons/react/24/outline';
 
 import { Logo } from 'components/Logo';
 
 export const Header = () => {
 	return (
-		<header className="mb-2 bg-white px-6 dark:bg-neutral-900">
+		<header className="mb-2 flex justify-between bg-white px-6 dark:bg-neutral-900">
 			<div className="flex items-center space-x-3">
 				<Bars3Icon
 					className="h-7 w-7 text-gray-500 dark:text-white"
 					title="Menu hamburguer"
 				/>
+
 				<Logo />
+			</div>
+
+			<div className="flex items-center  space-x-2">
+				<MagnifyingGlassIcon
+					className="h-6 w-6 text-gray-500 dark:text-white"
+					title="Search"
+				/>
+				<EllipsisVerticalIcon
+					className="h-6 w-6 text-gray-500 dark:text-white"
+					title="Settings"
+				/>
+				<button className="flex items-center border border-blue-500 py-1 px-2 text-sm font-bold text-blue-500 dark:border-blue-400 dark:text-blue-400">
+					<UserCircleIcon className="mr-1 h-6 w-6 text-blue-500 dark:text-blue-400" />
+					SIGN IN
+				</button>
 			</div>
 		</header>
 	);
