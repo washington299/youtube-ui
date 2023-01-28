0b82c135e9fc390fdc0e41f814647d303d6fed6b
Washington Campos <washingtoncampos9@gmail.com>
2023-01-27 22:54:44 -0300

    chore: set up babel preset typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: [
         '@babel/preset-env',
+        "next/babel", "@babel/preset-typescript",
         ['@babel/preset-react', {runtime: 'automatic'}],
     ],
 };
