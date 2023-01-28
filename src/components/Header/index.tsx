0ea2534ed4f44649d68157a74b8523a928fe2368
Washington Campos <washingtoncampos9@gmail.com>
2023-01-28 16:17:45 -0300

    feat: add Search component to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,26 +6,31 @@ import {
 } from '@heroicons/react/24/outline';
 
 import { Logo } from 'components/Logo';
+import { Search } from 'components/Search';
 
 export const Header = () => {
 	return (
-		<header className="mb-2 flex justify-between bg-white px-6 dark:bg-neutral-900">
+		<header className="mb-2 flex items-center justify-between bg-white px-6 dark:bg-neutral-900">
 			<div className="flex items-center space-x-3">
 				<Bars3Icon
-					className="h-7 w-7 text-gray-500 dark:text-white"
+					className="h-7 w-7 cursor-pointer text-gray-500 dark:text-white"
 					title="Menu hamburguer"
 				/>
 
 				<Logo />
 			</div>
 
-			<div className="flex items-center  space-x-2">
+			<div className="hidden flex-1 px-6 md:block">
+				<Search callback={() => {}} />
+			</div>
+
+			<div className="flex items-center space-x-2">
 				<MagnifyingGlassIcon
-					className="h-6 w-6 text-gray-500 dark:text-white"
+					className="h-6 w-6 text-gray-500 dark:text-white md:hidden"
 					title="Search"
 				/>
 				<EllipsisVerticalIcon
-					className="h-6 w-6 text-gray-500 dark:text-white"
+					className="h-6 w-6 cursor-pointer text-gray-500 dark:text-white"
 					title="Settings"
 				/>
 				<button className="flex items-center border border-blue-500 py-1 px-2 text-sm font-bold text-blue-500 dark:border-blue-400 dark:text-blue-400">
