54e4f72b5222901d5667df4a6fe6c86638367eeb
Washington Campos <washingtoncampos9@gmail.com>
2023-01-28 16:33:14 -0300

    test: ensure toggleMenu is called when menu hamburguer icon is clicked

--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { Header } from '.';
 
 describe('<Header />', () => {
 	it('Should render elements correctly', () => {
-		render(<Header />);
+		render(<Header toggleMenu={() => {}} />);
 
 		expect(screen.getByTitle(/Youtube Logo/i)).toBeInTheDocument();
 		expect(screen.getByTitle(/Menu hamburguer/i)).toBeInTheDocument();
@@ -14,5 +14,17 @@ describe('<Header />', () => {
 		expect(
 			screen.getByRole('button', { name: /Sign in/i }),
 		).toBeInTheDocument();
+	});
+
+	it('Should call toggleMenu function when menu hamburguer icon is clicked', () => {
+		const mockToggleMenu = jest.fn();
+
+		render(<Header toggleMenu={mockToggleMenu} />);
+
+		expect(mockToggleMenu).not.toBeCalled();
+
+		fireEvent.click(screen.getByTitle(/Menu hamburguer/i));
+
+		expect(mockToggleMenu).toBeCalledTimes(1);
 	});
 });
