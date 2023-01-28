79eb542463ff28bb6261088f665dbd5df8b47d3d
Washington Campos <washingtoncampos9@gmail.com>
2023-01-27 22:29:24 -0300

    feat: style input with clear icon

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,16 +7,20 @@ export const Search = () => {
 
 	return (
 		<form className="flex w-full items-center">
-			<div>
+			<div className="relative flex flex-1 items-center">
 				<input
 					type="text"
 					placeholder="Search"
-					className="flex-1 rounded-l-full border border-gray-200 py-2 px-4 outline-none focus:border-blue-600 dark:border-gray-700 dark:border-r-neutral-700 dark:bg-zinc-900 dark:text-white dark:focus:border-blue-400"
+					className="w-full rounded-l-full border border-gray-200 py-2 pl-6 pr-8 outline-none focus:border-blue-600 dark:border-gray-700 dark:border-r-neutral-700 dark:bg-zinc-900 dark:text-white dark:focus:border-blue-400"
 					value={inputValue}
 					onChange={e => setInputValue(e.target.value)}
 				/>
 				{inputValue.length > 0 && (
-					<XMarkIcon title="Clear" onClick={() => setInputValue('')} />
+					<XMarkIcon
+						className="absolute right-2 h-5 w-5 cursor-pointer text-gray-500 dark:text-white"
+						title="Clear"
+						onClick={() => setInputValue('')}
+					/>
 				)}
 			</div>
 			<button className="z-10 rounded-r-full border border-gray-200 bg-gray-100 py-2 px-5 hover:bg-gray-200 dark:border-neutral-700 dark:bg-neutral-700 dark:hover:bg-neutral-800">
