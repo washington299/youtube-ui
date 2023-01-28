701c0ee0a3744a055abfb1aa13c969d8cf453986
Washington Campos <washingtoncampos9@gmail.com>
2023-01-27 23:35:27 -0300

    feat: add callback function to Search component story

--- src/components/Search/stories.tsx
+++ src/components/Search/stories.tsx
@@ -6,11 +6,14 @@ import { Search } from '.';
 export default {
 	title: 'components/Search',
 	component: Search,
+	argTypes: {
+		callback: { action: 'clicked' },
+	},
 } as ComponentMeta<typeof Search>;
 
-const Template: ComponentStory<typeof Search> = () => (
+const Template: ComponentStory<typeof Search> = args => (
 	<div className="h-screen bg-white p-4 dark:bg-neutral-900">
-		<Search />
+		<Search {...args} />
 	</div>
 );
 
