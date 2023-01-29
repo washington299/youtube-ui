eca94dd25708c4607ed24704b8f3945ad576e6c5
Washington Campos <washingtoncampos9@gmail.com>
2023-01-29 18:44:05 -0300

    feat: create SettingsList story

---
+++ src/components/SettingsList/stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { SettingsList } from '.';
+
+export default {
+	title: 'components/SettingsList',
+	component: SettingsList,
+} as ComponentMeta<typeof SettingsList>;
+
+const Template: ComponentStory<typeof SettingsList> = () => (
+	<div className="h-screen bg-white p-4 dark:bg-neutral-900">
+		<SettingsList />
+	</div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
