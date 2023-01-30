f68aa2aadb46d4ffed02bdac4d400a8e3b6a7d01
Washington Campos <washingtoncampos9@gmail.com>
2023-01-30 14:14:56 -0300

    feat: hide check icon theme when theme is not active

--- src/components/SettingsList/Appearance/index.tsx
+++ src/components/SettingsList/Appearance/index.tsx
@@ -1,23 +1,46 @@
+import { useState } from 'react';
+import { CheckIcon } from '@heroicons/react/24/outline';
+
 type AppearanceProps = {
+	theme: string;
 	toggleTheme: (theme: string) => void;
 };
 
-export const Appearance = ({ toggleTheme }: AppearanceProps) => {
+export const Appearance = ({ theme, toggleTheme }: AppearanceProps) => {
+	const [currentTheme, setCurrentTheme] = useState(theme);
+
+	const handleToggleTheme = (newTheme: string) => {
+		setCurrentTheme(newTheme);
+		toggleTheme(newTheme);
+	};
+
 	return (
 		<div className="dark:text-white">
 			<p>Setting applies to this browser only</p>
 
 			<ul className="py-2">
 				<li
-					className="flex cursor-pointer items-center justify-between px-5 py-2 hover:bg-gray-50 dark:hover:bg-neutral-700"
-					onClick={() => toggleTheme('dark')}
+					className="flex cursor-pointer items-center px-5 py-2 hover:bg-gray-50 dark:hover:bg-neutral-700"
+					onClick={() => handleToggleTheme('dark')}
 				>
+					<CheckIcon
+						className={`mr-4 h-5 w-5 ${
+							currentTheme === 'dark' ? 'opacity-100' : 'opacity-0'
+						}`}
+						aria-label="dark theme"
+					/>
 					Dark theme
 				</li>
 				<li
-					className="flex cursor-pointer items-center justify-between px-5 py-2 hover:bg-gray-50 dark:hover:bg-neutral-700"
-					onClick={() => toggleTheme('light')}
+					className="flex cursor-pointer items-center px-5 py-2 hover:bg-gray-50 dark:hover:bg-neutral-700"
+					onClick={() => handleToggleTheme('light')}
 				>
+					<CheckIcon
+						className={`mr-4 h-5 w-5 ${
+							currentTheme === 'light' ? 'opacity-100' : 'opacity-0'
+						}`}
+						aria-label="light theme"
+					/>
 					Light theme
 				</li>
 			</ul>
