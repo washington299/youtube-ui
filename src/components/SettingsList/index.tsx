3dd1d6282d0172f460bbea6d4564ea8f7a535121
Washington Campos <washingtoncampos9@gmail.com>
2023-01-30 11:39:38 -0300

    feat: add dark mode colors to SettingsList component

--- src/components/SettingsList/index.tsx
+++ src/components/SettingsList/index.tsx
@@ -6,13 +6,13 @@ export const SettingsList = () => {
 	const settingsItems = useSettingsItems();
 
 	return (
-		<div className="w-80 divide-y border border-gray-200 bg-white">
+		<div className="w-80 divide-y border border-gray-200 bg-white dark:divide-gray-600 dark:border-gray-600 dark:bg-neutral-900">
 			{settingsItems.map((list, index) => (
-				<ul key={index} className="py-2">
+				<ul key={index} className="py-2 dark:text-white">
 					{list.map(item => (
 						<li
 							key={item.text}
-							className="flex cursor-pointer items-center justify-between px-5 py-2 hover:bg-gray-50"
+							className="flex cursor-pointer items-center justify-between px-5 py-2 hover:bg-gray-50 dark:hover:bg-neutral-700"
 						>
 							<div className="flex items-center">
 								{item.icon}
