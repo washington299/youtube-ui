5693d6704da13cf69900405b4e1f66c035bc2548
Washington Campos <washingtoncampos9@gmail.com>
2023-01-30 15:18:47 -0300

    test: ensure SettingsList renders correct active children when an item is clicked

--- src/components/SettingsList/test.tsx
+++ src/components/SettingsList/test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { SettingsList } from '.';
 
@@ -9,5 +9,19 @@ describe('<SettingsList />', () => {
 		expect(screen.getAllByRole('list')).toHaveLength(4);
 		expect(screen.getAllByRole('listitem')).toHaveLength(9);
 		expect(screen.getByText(/Your data in Youtube/i)).toBeInTheDocument();
+	});
+
+	it('Should render correct children element based on item click', () => {
+		render(<SettingsList />);
+
+		fireEvent.click(screen.getByText(/Appearance/i));
+
+		expect(screen.getByText(/Dark theme/i)).toBeInTheDocument();
+		expect(screen.getByText(/Light theme/i)).toBeInTheDocument();
+
+		fireEvent.click(screen.getByLabelText(/Arrow back/i));
+
+		expect(screen.getAllByRole('list')).toHaveLength(4);
+		expect(screen.getAllByRole('listitem')).toHaveLength(9);
 	});
 });
