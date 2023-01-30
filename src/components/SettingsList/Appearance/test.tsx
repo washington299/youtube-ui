74164a8edc022c279c06d04d445f9bacb93014d9
Washington Campos <washingtoncampos9@gmail.com>
2023-01-30 14:15:56 -0300

    test: ensure check icon theme renders correctly based on active theme

--- src/components/SettingsList/Appearance/test.tsx
+++ src/components/SettingsList/Appearance/test.tsx
@@ -1,19 +1,36 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 
+import { renderWithTailwind } from 'utils/tests/helpers';
+
 import { Appearance } from '.';
 
 describe('<Appearance />', () => {
 	it('Should render correct items', () => {
-		render(<Appearance toggleTheme={() => {}} />);
+		render(<Appearance theme="light" toggleTheme={() => {}} />);
 
 		expect(screen.getByText(/Dark theme/i)).toBeInTheDocument();
 		expect(screen.getByText(/Light theme/i)).toBeInTheDocument();
 	});
 
+	it('Should render correct selected theme based on active theme', () => {
+		renderWithTailwind(<Appearance theme="light" toggleTheme={() => {}} />);
+
+		const darkCheckedIcon = screen.getByLabelText(/Dark theme/i);
+		const lightCheckedIcon = screen.getByLabelText(/Light theme/i);
+
+		expect(darkCheckedIcon).not.toBeVisible();
+		expect(lightCheckedIcon).toBeVisible();
+
+		fireEvent.click(screen.getByText(/Dark theme/i));
+
+		expect(lightCheckedIcon).not.toBeVisible();
+		expect(darkCheckedIcon).toBeVisible();
+	});
+
 	it('Should call toggleTheme with correct theme value when any theme is selected', () => {
 		const mockToggleTheme = jest.fn();
 
-		render(<Appearance toggleTheme={mockToggleTheme} />);
+		render(<Appearance theme="light" toggleTheme={mockToggleTheme} />);
 
 		expect(mockToggleTheme).not.toBeCalled();
 
