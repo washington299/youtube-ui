a22a642b028ae72e20a819c4b1642ae183a2fc1d
Washington Campos <washingtoncampos9@gmail.com>
2023-01-31 19:30:52 -0300

    feat: create languagesList data

---
+++ src/components/SettingsList/Language/languagesList.ts
@@ -0,0 +1,94 @@
+export const languagesList = [
+	{
+		language: 'Afrikaans',
+		key: 'afrikaans',
+	},
+	{
+		language: 'Bahasa Indonesia',
+		key: 'bahasa_indonesia',
+	},
+	{
+		language: 'Bahasa Malaysia',
+		key: 'bahasa_malaysia',
+	},
+	{
+		language: 'Bosanski',
+		key: 'bosanski',
+	},
+	{
+		language: 'Català',
+		key: 'catala',
+	},
+	{
+		language: 'Dansk',
+		key: 'dansk',
+	},
+	{
+		language: 'Deutsch',
+		key: 'deutsch',
+	},
+	{
+		language: 'Eesti',
+		key: 'eesti',
+	},
+	{
+		language: 'English (India)',
+		key: 'english_india',
+	},
+	{
+		language: 'English (UK)',
+		key: 'english_uk',
+	},
+	{
+		language: 'English (US)',
+		key: 'english_us',
+	},
+	{
+		language: 'Español (España)',
+		key: 'espanol_espana',
+	},
+	{
+		language: 'Español (Latinoamérica)',
+		key: 'espanol_latinoamerica',
+	},
+	{
+		language: 'Español (US)',
+		key: 'espanol_us',
+	},
+	{
+		language: 'Euskara',
+		key: 'euskara',
+	},
+	{
+		language: 'Filipino',
+		key: 'filipino',
+	},
+	{
+		language: 'Français',
+		key: 'francais',
+	},
+	{
+		language: 'Français (Canada)',
+		key: 'francais_canada',
+	},
+	{
+		language: 'Galego',
+		key: 'galego',
+	},
+	{
+		language: 'Italiano',
+		key: 'italiano',
+	},
+	{
+		language: 'Nederlands',
+		key: 'nederlands',
+	},
+	{
+		language: 'Português',
+		key: 'portugues',
+	},
+	{
+		language: 'Português (Brasil)',
+		key: 'portugues_brasil',
+	},
+];
