b623d4ee31a93487a3a1ddf092df6c5dc714c56e
Washington Campos <washingtoncampos9@gmail.com>
2023-01-31 22:51:20 -0300

    feat: add args to Switch story

--- src/components/Switch/stories.tsx
+++ src/components/Switch/stories.tsx
@@ -6,13 +6,16 @@ import { Switch } from '.';
 export default {
 	title: 'components/Switch',
 	component: Switch,
+	argTypes: { toggleSwitch: { action: 'clicked' } },
 } as ComponentMeta<typeof Switch>;
 
-const Template: ComponentStory<typeof Switch> = () => (
+const Template: ComponentStory<typeof Switch> = args => (
 	<div className="h-screen bg-white p-4 dark:bg-black">
-		<Switch />
+		<Switch {...args} />
 	</div>
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+	label: 'Click here to toggle',
+};
