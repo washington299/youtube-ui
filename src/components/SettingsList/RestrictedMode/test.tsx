24928eac0ddd3ee90f3e8b9ad666230204d13f9f
Washington Campos <washingtoncampos9@gmail.com>
2023-02-01 12:55:02 -0300

    test: ensure toggleSwitch function is called with correct value

--- src/components/SettingsList/RestrictedMode/test.tsx
+++ src/components/SettingsList/RestrictedMode/test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { RestrictedMode } from '.';
 
 describe('<RestrictedMode />', () => {
 	it('Should renders correctly texts and elements', () => {
-		render(<RestrictedMode />);
+		render(<RestrictedMode toggleSwitch={() => {}} />);
 
 		expect(
 			screen.getByText(
@@ -18,8 +18,24 @@ describe('<RestrictedMode />', () => {
 	});
 
 	it('Should mark switch checked when checked prop is true', () => {
-		render(<RestrictedMode checked />);
+		render(<RestrictedMode checked toggleSwitch={() => {}} />);
 
 		expect(screen.getByRole('checkbox')).toBeChecked();
+	});
+
+	it('Should call toggleSwitch function with correct value', () => {
+		const mockToggleSwitch = jest.fn();
+
+		render(<RestrictedMode toggleSwitch={mockToggleSwitch} />);
+
+		expect(mockToggleSwitch).not.toBeCalled();
+
+		fireEvent.click(screen.getByRole('checkbox'));
+
+		expect(mockToggleSwitch).toHaveBeenCalledWith('on');
+
+		fireEvent.click(screen.getByRole('checkbox'));
+
+		expect(mockToggleSwitch).toHaveBeenCalledWith('off');
 	});
 });
