3b5ce6a9fd81dec47cd30e8c683bd02c5de8528a
Washington Campos <washingtoncampos9@gmail.com>
2023-02-01 15:17:55 -0300

    feat: create locations list

---
+++ src/components/SettingsList/Location/locationsList.ts
@@ -0,0 +1,110 @@
+export const LocationsList = [
+	{
+		location: 'Algeria',
+		key: 'algeria',
+	},
+	{
+		location: 'Argentina',
+		key: 'argentina',
+	},
+	{
+		location: 'Australia',
+		key: 'australia',
+	},
+	{
+		location: 'Austria',
+		key: 'austria',
+	},
+	{
+		location: 'Bangladesh',
+		key: 'bangladesh',
+	},
+	{
+		location: 'Belgium',
+		key: 'belgium',
+	},
+	{
+		location: 'Bolivia',
+		key: 'bolivia',
+	},
+	{
+		location: 'Brazil',
+		key: 'brazil',
+	},
+	{
+		location: 'Canada',
+		key: 'canada',
+	},
+	{
+		location: 'Chile',
+		key: 'chile',
+	},
+	{
+		location: 'Colombia',
+		key: 'colombia',
+	},
+	{
+		location: 'Denmark',
+		key: 'denmark',
+	},
+	{
+		location: 'Dominican Republic',
+		key: 'dominican_republic',
+	},
+	{
+		location: 'Ecuador',
+		key: 'ecuador',
+	},
+	{
+		location: 'Egypt',
+		key: 'egypt',
+	},
+	{
+		location: 'Finland',
+		key: 'finland',
+	},
+	{
+		location: 'France',
+		key: 'france',
+	},
+	{
+		location: 'Germany',
+		key: 'germany',
+	},
+	{
+		location: 'Greece',
+		key: 'greece',
+	},
+	{
+		location: 'Honduras',
+		key: 'honduras',
+	},
+	{
+		location: 'Hong Kong',
+		key: 'hong_kong',
+	},
+	{
+		location: 'India',
+		key: 'india',
+	},
+	{
+		location: 'Japan',
+		key: 'japan',
+	},
+	{
+		location: 'Mexico',
+		key: 'mexico',
+	},
+	{
+		location: 'Netherlands',
+		key: 'netherlands',
+	},
+	{
+		location: 'Portugal',
+		key: 'portugal',
+	},
+	{
+		location: 'Spain',
+		key: 'spain',
+	},
+];
