8c7f71ff16eda4d2436b727b00e0eef87061482f
Washington Campos <washingtoncampos9@gmail.com>
2023-02-01 15:23:06 -0300

    test: ensure changeLocation funciton is called with correct value

--- src/components/SettingsList/Location/test.tsx
+++ src/components/SettingsList/Location/test.tsx
@@ -4,13 +4,13 @@ import { Location } from '.';
 
 describe('<Location />', () => {
 	it('Should render locations list correctly', () => {
-		render(<Location location="brazil" />);
+		render(<Location location="brazil" changeLocation={() => {}} />);
 
 		expect(screen.getAllByRole('listitem')).toHaveLength(27);
 	});
 
-	it('Should render CheckIcon on default language and toggle between languages', () => {
-		render(<Location location="brazil" />);
+	it('Should render CheckIcon on default language and toggle between locations', () => {
+		render(<Location location="brazil" changeLocation={() => {}} />);
 
 		expect(screen.getByLabelText(/brazil/i).getAttribute('aria-hidden')).toBe(
 			'false',
@@ -27,5 +27,17 @@ describe('<Location />', () => {
 		expect(screen.getByLabelText(/portugal/i).getAttribute('aria-hidden')).toBe(
 			'false',
 		);
+	});
+
+	it('Should call changeLanguage function with correct language', () => {
+		const mockChangeLocation = jest.fn();
+
+		render(<Location location="brazil" changeLocation={mockChangeLocation} />);
+
+		expect(mockChangeLocation).not.toBeCalled();
+
+		fireEvent.click(screen.getByText('Portugal'));
+
+		expect(mockChangeLocation).toBeCalledWith('portugal');
 	});
 });
