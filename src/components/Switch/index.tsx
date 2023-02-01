26ceacbaa1c4bae2dc6dd6d4d66012e31f823a29
Washington Campos <washingtoncampos9@gmail.com>
2023-01-31 23:07:23 -0300

    feat: adjust switch styles

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -29,7 +29,7 @@ export const Switch = ({
 				className="peer sr-only"
 				onChange={handleChangeCheckbox}
 			/>
-			<div className="peer relative mx-2 h-4 w-10 rounded-full bg-gray-200 after:absolute after:top-[-2px] after:left-[1px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800" />
+			<div className="peer relative mx-2 h-4 w-10 rounded-full bg-gray-300 after:absolute after:top-[-2px] after:left-[1px] after:h-5 after:w-5 after:rounded-full after:border after:border-black after:bg-black after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-blue-600 peer-checked:after:bg-blue-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:after:border-white dark:after:bg-white dark:peer-focus:ring-blue-800" />
 		</label>
 	);
 };
