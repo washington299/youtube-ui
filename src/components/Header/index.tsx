29db9aa87dcc129d6a7519f82208d02110626f58
Washington Campos <washingtoncampos9@gmail.com>
2023-02-02 15:49:18 -0300

    refactor: add SettingsDropdown to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import {
 	Bars3Icon,
 	MagnifyingGlassIcon,
-	EllipsisVerticalIcon,
 	UserCircleIcon,
 } from '@heroicons/react/24/outline';
 
 import { Logo } from 'components/Logo';
 import { Search } from 'components/Search';
+import { SettingsDropdown } from 'components/SettingsDropdown';
 
 type HeaderProps = {
 	toggleMenu: () => void;
@@ -38,10 +38,7 @@ export const Header = ({ toggleMenu }: HeaderProps) => {
 					className="h-6 w-6 text-gray-500 dark:text-white md:hidden"
 					title="Search"
 				/>
-				<EllipsisVerticalIcon
-					className="h-6 w-6 cursor-pointer text-gray-500 dark:text-white"
-					title="Settings"
-				/>
+				<SettingsDropdown />
 				<button className="flex items-center border border-blue-500 py-1 px-2 text-sm font-bold text-blue-500 dark:border-blue-400 dark:text-blue-400">
 					<UserCircleIcon className="mr-1 h-6 w-6 text-blue-500 dark:text-blue-400" />
 					SIGN IN
