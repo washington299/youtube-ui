f22e9be5e2a72663f646aa1af4ebcc4fc8deff94
Washington Campos <washingtoncampos9@gmail.com>
2023-02-03 17:51:57 -0300

    feat: add iconSolid on sidebarClosed items

--- src/components/SidebarClosed/sidebarClosedItems.tsx
+++ src/components/SidebarClosed/sidebarClosedItems.tsx
@@ -1,35 +1,85 @@
 import {
-	HomeIcon,
-	FilmIcon,
-	PlayCircleIcon,
-	ArchiveBoxArrowDownIcon,
-	ArrowPathIcon,
+	HomeIcon as HomeIconOutline,
+	FilmIcon as FilmIconOutline,
+	PlayCircleIcon as PlayCircleIconOutline,
+	ArchiveBoxArrowDownIcon as ArchiveBoxArrowDownIconOutline,
+	ArrowPathIcon as ArrowPathIconOutline,
 } from '@heroicons/react/24/outline';
+import {
+	HomeIcon as HomeIconSolid,
+	FilmIcon as FilmIconSolid,
+	PlayCircleIcon as PlayCircleIconSolid,
+	ArchiveBoxArrowDownIcon as ArchiveBoxArrowDownIconSolid,
+	ArrowPathIcon as ArrowPathIconSolid,
+} from '@heroicons/react/24/solid';
 
 export const sidebarClosedItems = [
 	{
 		title: 'Home',
-		icon: <HomeIcon className="h-6 w-6" />,
+		iconOutline: (
+			<HomeIconOutline className="h-6 w-6" aria-label="Home icon outline" />
+		),
+		iconSolid: (
+			<HomeIconSolid className="h-6 w-6" aria-label="Home icon solid" />
+		),
 		link: '/',
 	},
 	{
 		title: 'Shorts',
-		icon: <FilmIcon className="h-6 w-6" />,
+		iconOutline: (
+			<FilmIconOutline className="h-6 w-6" aria-label="Film icon outline" />
+		),
+		iconSolid: (
+			<FilmIconSolid className="h-6 w-6" aria-label="Film icon solid" />
+		),
 		link: '/shorts',
 	},
 	{
 		title: 'Subscriptions',
-		icon: <PlayCircleIcon className="h-6 w-6" />,
+		iconOutline: (
+			<PlayCircleIconOutline
+				className="h-6 w-6"
+				aria-label="PlayCircle icon outline"
+			/>
+		),
+		iconSolid: (
+			<PlayCircleIconSolid
+				className="h-6 w-6"
+				aria-label="PlayCircle icon solid"
+			/>
+		),
 		link: '/subscriptions',
 	},
 	{
 		title: 'Library',
-		icon: <ArchiveBoxArrowDownIcon className="h-6 w-6" />,
+		iconOutline: (
+			<ArchiveBoxArrowDownIconOutline
+				className="h-6 w-6"
+				aria-label="ArchiveBox icon outline"
+			/>
+		),
+		iconSolid: (
+			<ArchiveBoxArrowDownIconSolid
+				className="h-6 w-6"
+				aria-label="ArchiveBox icon solid"
+			/>
+		),
 		link: '/library',
 	},
 	{
 		title: 'History',
-		icon: <ArrowPathIcon className="h-6 w-6" />,
+		iconOutline: (
+			<ArrowPathIconOutline
+				className="h-6 w-6"
+				aria-label="ArrowPath icon outline"
+			/>
+		),
+		iconSolid: (
+			<ArrowPathIconSolid
+				className="h-6 w-6"
+				aria-label="ArrowPath icon solid"
+			/>
+		),
 		link: '/history',
 	},
 ];
