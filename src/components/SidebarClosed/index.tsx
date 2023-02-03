07d46c2f68bed71ce5bdef051e0efc3a2429d4af
Washington Campos <washingtoncampos9@gmail.com>
2023-02-03 17:54:20 -0300

    feat: change icon based on currentPage

--- src/components/SidebarClosed/index.tsx
+++ src/components/SidebarClosed/index.tsx
@@ -2,18 +2,32 @@ import Link from 'next/link';
 
 import { sidebarClosedItems } from './sidebarClosedItems';
 
-export const SidebarClosed = () => {
+export type SidebarProps = {
+	currentPage: string;
+};
+
+export const SidebarClosed = ({ currentPage }: SidebarProps) => {
 	return (
 		<nav className="h-screen w-20 bg-white">
 			<ul className="py-2">
-				{sidebarClosedItems.map(({ icon, title, link }) => (
-					<Link key={title} href={link}>
-						<li className="flex flex-col items-center py-3 hover:bg-gray-100">
-							{icon}
-							<span className="text-[10px]">{title}</span>
-						</li>
-					</Link>
-				))}
+				{sidebarClosedItems.map(({ iconOutline, iconSolid, title, link }) => {
+					const isCurrentPage = currentPage === link;
+
+					return (
+						<Link key={title} href={link}>
+							<li
+								className={`flex flex-col items-center py-3 hover:bg-gray-100 ${
+									isCurrentPage && 'bg-gray-100'
+								}`}
+							>
+								{isCurrentPage ? iconSolid : iconOutline}
+								<span className={`text-[10px] ${isCurrentPage && 'font-bold'}`}>
+									{title}
+								</span>
+							</li>
+						</Link>
+					);
+				})}
 			</ul>
 		</nav>
 	);
