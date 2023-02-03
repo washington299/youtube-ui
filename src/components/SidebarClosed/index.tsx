c5d69a1a09cf6838b0c929d79a7835a0c55b14a1
Washington Campos <washingtoncampos9@gmail.com>
2023-02-03 17:06:43 -0300

    chore: add styles to sidebarClosed

--- src/components/SidebarClosed/index.tsx
+++ src/components/SidebarClosed/index.tsx
@@ -4,13 +4,13 @@ import { sidebarClosedItems } from './sidebarClosedItems';
 
 export const SidebarClosed = () => {
 	return (
-		<nav>
-			<ul>
+		<nav className="h-screen w-20 bg-white">
+			<ul className="py-2">
 				{sidebarClosedItems.map(({ icon, title, link }) => (
 					<Link key={title} href={link}>
-						<li>
+						<li className="flex flex-col items-center py-3 hover:bg-gray-100">
 							{icon}
-							{title}
+							<span className="text-[10px]">{title}</span>
 						</li>
 					</Link>
 				))}
