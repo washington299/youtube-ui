e2eae011e0c15818a863b9acf08305ffb70b6064
Washington Campos <washingtoncampos9@gmail.com>
2023-02-03 17:56:17 -0300

    feat: create SidebarClosed story

---
+++ src/components/SidebarClosed/stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { SidebarClosed } from '.';
+
+export default {
+	title: 'components/SidebarClosed',
+	component: SidebarClosed,
+	args: { currentPage: '/' },
+} as ComponentMeta<typeof SidebarClosed>;
+
+const Template: ComponentStory<typeof SidebarClosed> = args => (
+	<div className="h-screen bg-slate-100 dark:bg-neutral-900">
+		<SidebarClosed {...args} />
+	</div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
