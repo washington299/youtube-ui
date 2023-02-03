c51abbdf784bc9f621d50cf260399cc714dd4d88
Washington Campos <washingtoncampos9@gmail.com>
2023-02-03 17:54:44 -0300

    test: ensure icon is render correctly on currentPage

--- src/components/SidebarClosed/test.tsx
+++ src/components/SidebarClosed/test.tsx
@@ -1,12 +1,21 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { SidebarClosed } from '.';
 
 describe('<SidebarClosed />', () => {
 	it('Should render elements items list correctly', () => {
-		render(<SidebarClosed />);
+		render(<SidebarClosed currentPage="/" />);
 
 		expect(screen.getByText(/Home/i)).toBeInTheDocument();
 		expect(screen.getAllByRole('listitem')).toHaveLength(5);
+	});
+
+	it('Should render correct icon when is current page', () => {
+		render(<SidebarClosed currentPage="/" />);
+
+		expect(
+			screen.queryByLabelText(/Home icon outline/i),
+		).not.toBeInTheDocument();
+		expect(screen.getByLabelText(/Home icon solid/i)).toBeInTheDocument();
 	});
 });
