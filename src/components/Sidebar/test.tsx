91b2150cef313fbf485e4a4cec87d69e50ce2625
Washington Campos <washingtoncampos9@gmail.com>
2023-02-06 14:58:14 -0300

    test: ensure sidebar renders correct active item based on pathname

--- src/components/Sidebar/test.tsx
+++ src/components/Sidebar/test.tsx
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react';
 
 import { Sidebar } from '.';
 
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+useRouter.mockImplementation(() => ({
+	pathname: '/shorts',
+}));
+
 describe('<Sidebar />', () => {
 	it('Should render sidebar closed if isOpen prop is false', () => {
 		render(<Sidebar isOpen={false} />);
@@ -15,5 +20,14 @@ describe('<Sidebar />', () => {
 		expect(
 			screen.getByRole('button', { name: /Sign in/i }),
 		).toBeInTheDocument();
+	});
+
+	it('Should render correct active sidebar item based on pathname', () => {
+		render(<Sidebar isOpen />);
+
+		expect(screen.getByLabelText(/Shorts icon solid/i)).toBeInTheDocument();
+		expect(
+			screen.queryByLabelText(/Shorts icon outline/i),
+		).not.toBeInTheDocument();
 	});
 });
